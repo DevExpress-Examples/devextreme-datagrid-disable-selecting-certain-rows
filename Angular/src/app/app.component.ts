cc7300c14da0f8c89a4e9b9d3beffc9e35d36359
Mark Allen Ramirez <markallenramirez@gmail.com>
2025-08-20 18:35:43 +0800

    Fix Angular

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,20 +1,94 @@
-import { Component } from '@angular/core';
-import { ClickEvent } from 'devextreme/ui/button';
+import { Component, ViewChild } from '@angular/core';
+import { DxDataGridComponent } from 'devextreme-angular';
+
+import dxDataGrid, { SelectionChangedEvent, ContentReadyEvent } from 'devextreme/ui/data_grid';
+import dxCheckBox, {
+  InitializedEvent,
+  ValueChangedEvent,
+} from 'devextreme/ui/check_box';
+import { SalesItem, Service } from './app.service';
+import { EditorPreparingEventEx } from './app.types';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [Service],
 })
 export class AppComponent {
-  title = 'Angular';
+  @ViewChild(DxDataGridComponent, { static: false })
+    dataGrid!: DxDataGridComponent;
 
-  counter = 0;
+  dataSource: SalesItem[];
 
-  buttonText = 'Click count: 0';
+  selectAllCheckBox!: dxCheckBox;
 
-  onClick(e: ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
+  checkBoxUpdating = false;
+
+  constructor(service: Service) {
+    this.dataSource = service.getSales();
+    this.onEditorPreparing = this.onEditorPreparing.bind(this);
+    this.onSelectionChanged = this.onSelectionChanged.bind(this);
+  }
+
+  isSelectable(item: SalesItem): boolean {
+    return item.approved;
+  }
+
+  isSelectAll = (dataGrid: dxDataGrid): boolean | undefined => {
+    const selectableItems = this.dataSource.filter(this.isSelectable);
+    const selectedRowKeys = dataGrid.option('selectedRowKeys');
+
+    if (!selectedRowKeys?.length) {
+      return false;
+    }
+    return selectedRowKeys.length >= selectableItems.length ? true : undefined;
+  };
+
+  onEditorPreparing(e: EditorPreparingEventEx<SalesItem, number>): void {
+    if (e.type !== 'selection') return;
+
+    if (e.parentType === 'dataRow' && e.row && !this.isSelectable(e.row.data)) e.editorOptions.disabled = true;
+    if (e.parentType === 'headerRow') {
+      const dataGrid = e.component;
+      e.editorOptions.value = this.isSelectAll(dataGrid);
+      e.editorOptions.onInitialized = (e: InitializedEvent): void => {
+        if (e.component) this.selectAllCheckBox = e.component;
+      };
+      e.editorOptions.onValueChanged = (e: ValueChangedEvent): void => {
+        if (!e.event) {
+          if (e.previousValue && !this.checkBoxUpdating) {
+            e.component.option('value', e.previousValue);
+          }
+          return;
+        }
+
+        if (this.isSelectAll(dataGrid) === e.value) {
+          return;
+        }
+        const result = e.value ? dataGrid.selectAll() : dataGrid.deselectAll();
+        // eslint-disable-next-line no-console
+        result.catch((error) => { console.error(error); });
+        e.event.preventDefault();
+      };
+    }
+  }
+
+  async onSelectionChanged(e: SelectionChangedEvent<SalesItem, number>): Promise<void> {
+    const deselectRowKeys: number[] = [];
+    const dataGrid = e.component;
+    e.selectedRowsData.forEach((item) => {
+      if (!this.isSelectable(item)) deselectRowKeys.push(dataGrid.keyOf(item));
+    });
+    if (deselectRowKeys.length) {
+      await dataGrid.deselectRows(deselectRowKeys);
+    }
+    this.checkBoxUpdating = true;
+    this.selectAllCheckBox.option('value', this.isSelectAll(dataGrid));
+    this.checkBoxUpdating = false;
+  }
+
+  onContentReady(e: ContentReadyEvent): void {
+    this.selectAllCheckBox.option('value', this.isSelectAll(e.component));
   }
 }
